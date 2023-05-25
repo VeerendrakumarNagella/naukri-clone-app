53cf3c72981145ead7099c3bca6efcfafcc63b91
Lakshmi Kadasani <laxmikadasani577@gmail.com>
2023-05-25 23:22:25 +0530

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Header from "./components/Header";
 
 const App = () => {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const getAuth = window.localStorage.getItem("auth");
+    if (getAuth === "true") {
+      setAuth(true);
+    }
+  }, []);
+
   return (
     <div>
       <BrowserRouter>
+        <Header auth={auth} setAuth={setAuth} />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={auth ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/login"
+            element={!auth ? <Login setAuth={setAuth} /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/register"
+            element={!auth ? <Register /> : <Navigate to="/" />}
+          />
         </Routes>
       </BrowserRouter>
-      {/* <Home /> */}
     </div>
   );
 };
